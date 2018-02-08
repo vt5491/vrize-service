dc8ec35e31f0b8e00b85d522b0a4dba8402da744
Vince Turner <vt5491@gmail.com>
2018-02-08 00:01:19 -0800

    Add query support to index.html and allow for basic json servicing to remote window machine.

--- app/controllers/examples_controller.rb
+++ app/controllers/examples_controller.rb
@@ -1,3 +1,5 @@
+require 'active_record'
+
 class ExamplesController < ApplicationController
   before_action :set_example, only: [:show, :edit, :update, :destroy]
 
@@ -5,12 +7,52 @@ class ExamplesController < ApplicationController
   # GET /examples.json
   def index
     p "index: params=#{params}"
-    @examples = Example.all
+    # @examples = Example.all
     # @examples = if params[:name]
     #   Example.where('name LIKE ?', "%#{params[:name]}%")
     # else
     #   Example.all
     # end
+
+    p "params[:name]=#{params[:name]}"
+
+    cat_q = nil 
+    if params[:category] && params[:category] != 'any'
+      # cat_q = 'category = ?', "%#{params[:category]}%"
+      # cat_q = "category = #{params[:category]}"
+      # cat_q = sanitize_sql_array(["category=?", params[:category]])
+      clean_category = ActionController::Base.helpers.sanitize(params[:category])
+      cat_q = "category = '#{clean_category}'"
+    end
+
+    name_q = nil 
+    if params[:name]
+      # name_q += 'category = ?', "%#{params[:category]}%"
+      # name_q = "name LIKE #{params[:name]}"
+      # name_q = "name LIKE " +  params[:name]
+      # name_q = ActiveRecord::Sanitizaion::sanitize_sql_array(["name LIKE ?", params[:name]])
+      # name_q = ActionController::Base.helpers.sanitize([ "name LIKE ?", params[:name] ])
+      clean_name = ActionController::Base.helpers.sanitize(params[:name])
+      name_q = "name LIKE '%#{clean_name}%'"
+    end
+
+    q = nil 
+    if cat_q && name_q
+      q = name_q + " AND " + cat_q
+    elsif cat_q
+      q = cat_q
+    elsif name_q
+      q = name_q
+    end
+
+    p "q=#{q}"
+
+    @examples = if q
+      Example.where(q)
+    else
+      Example.all
+    end
+
   end
 
   # GET /examples/1
--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,15 @@ module VrizeService
     #vt add
     # make timestamps in the db be local time not gmt.
     config.active_record.default_timezone = :local
+
+    # try to make it so when we access this service from a remote machine we don't
+    # get cors issues
+    config.action_dispatch.default_headers = {
+      # 'Access-Control-Allow-Origin' => 'http://my-web-service-consumer-site.com',
+      # Note: you need to specify the address of the client, not that of the rails server
+      'Access-Control-Allow-Origin' => 'http://localhost:4200',
+      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+    }
     #vt end
   end
 end
